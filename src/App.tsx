5eb817fe02c0943694c026274ef0cbb5da369b3d
ArtemShalamov <iart1756@gmail.com>
2024-06-06 15:43:48 +0500

    added cypress msw

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Example } from './Example';
 
 function App() {
   return (
@@ -9,6 +10,8 @@ function App() {
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.tsx</code> and save to reload.
+          <Example/>
+
         </p>
         <a
           className="App-link"
--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { startWorker } from './mocks/ghapi';
+
+if (process.env.NODE_ENV === 'development') {
+  startWorker();
+}
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
